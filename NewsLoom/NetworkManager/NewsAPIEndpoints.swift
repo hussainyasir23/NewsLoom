3fab7d7bc0f5536e2736a66c7ba614b8ce491fef
Yasir Hussain <hussainyasir23@gmail.com>
2024-10-14 22:14:40 +0530

    Update endpoints to include other params

--- NewsLoom/NetworkManager/NewsAPIEndpoints.swift
+++ NewsLoom/NetworkManager/NewsAPIEndpoints.swift
@@ -8,9 +8,10 @@
 import Foundation
 
 enum NewsAPIEndpoints: Endpoint {
-    case topHeadlines(page: Int)
-    case everything(query: String, page: Int)
-    case sources
+    
+    case topHeadlines(country: NewsCountry, page: Int, category: NewsCategory?)
+    case everything(query: String, page: Int, sortBy: SortBy?)
+    case sources(category: NewsCategory?, language: NewsLanguage?, country: NewsCountry?)
     
     var baseURL: String {
         return "https://newsapi.org"
@@ -31,20 +32,44 @@ enum NewsAPIEndpoints: Endpoint {
         return .get
     }
     
-    var headers: [String : String]? {
+    var headers: [String: String]? {
         return nil
     }
     
-    var parameters: [String : Any]? {
+    var parameters: [String: Any]? {
         switch self {
-        case .topHeadlines(let page):
-            return ["country": "us",
-                    "page": page]
-        case .everything(let query, let page):
-            return ["q": query,
-                    "page": page]
-        case .sources:
-            return nil
+        case .topHeadlines(let country, let page, let category):
+            var params: [String: Any] = [
+                "country": country.code,
+                "page": page
+            ]
+            if let category = category {
+                params["category"] = category.id
+            }
+            return params
+            
+        case .everything(let query, let page, let sortBy):
+            var params: [String: Any] = [
+                "q": query,
+                "page": page
+            ]
+            if let sortBy = sortBy {
+                params["sortBy"] = sortBy.rawValue
+            }
+            return params
+            
+        case .sources(let category, let language, let country):
+            var params: [String: Any] = [:]
+            if let category = category {
+                params["category"] = category.id
+            }
+            if let language = language {
+                params["language"] = language.code
+            }
+            if let country = country {
+                params["country"] = country.code
+            }
+            return params.isEmpty ? nil : params
         }
     }
 }

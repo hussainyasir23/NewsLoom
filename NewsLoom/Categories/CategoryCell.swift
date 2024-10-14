67d8e0f269cacba6c93faa09b3e325d4c2ac7237
Yasir Hussain <hussainyasir23@gmail.com>
2024-10-14 21:46:32 +0530

    Add Categories View

---
+++ NewsLoom/Categories/CategoryCell.swift
@@ -0,0 +1,63 @@
+//
+//  CategoryCell.swift
+//  NewsLoom
+//
+//  Created by Yasir on 14/10/24.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = "CategoryCell"
+    
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .label
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        contentView.addSubview(iconImageView)
+        contentView.addSubview(nameLabel)
+        
+        NSLayoutConstraint.activate([
+            iconImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            iconImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor, constant: -20),
+            iconImageView.widthAnchor.constraint(equalToConstant: 50),
+            iconImageView.heightAnchor.constraint(equalToConstant: 50),
+            
+            nameLabel.topAnchor.constraint(equalTo: iconImageView.bottomAnchor, constant: 10),
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
+            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5),
+            nameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10)
+        ])
+        
+        contentView.backgroundColor = .systemGray6
+        contentView.layer.cornerRadius = 10
+    }
+    
+    func configure(with category: Category) {
+        iconImageView.image = UIImage(systemName: category.icon)
+        nameLabel.text = category.name
+    }
+}
